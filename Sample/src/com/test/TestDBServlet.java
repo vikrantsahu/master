36e733b17d3272a499439d75ac74f4f1d64d884a
rosmi <rosmi@cloudboxonline.com>
2014-10-11 23:21:00 +0530

    Fix # sterror

--- Sample/src/com/test/TestDBServlet.java
+++ Sample/src/com/test/TestDBServlet.java
@@ -15,7 +15,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-//import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class TestDBServlet
@@ -41,7 +41,7 @@ public class TestDBServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		//HttpSession session = request.getSession(true);
+		HttpSession session = request.getSession(true);
 
       
 
